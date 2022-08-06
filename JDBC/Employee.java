6fe0c800d4eae5f9427a2ea2c634f3cde9799495
Saurabh  Nagendra Dubey <35058497+saurabhdubey836@users.noreply.github.com>
2022-08-06 12:01:40 +0530

    Add files via upload
    Mini project in DAO java

---
+++ JDBC/Employee.java
@@ -0,0 +1,36 @@
+package com.saurabh.jdbc;
+
+public class Employee extends Object{
+	private int eno;
+	private String ename;
+	private float esal;
+	private String eadd;
+	
+	Employee(){
+		
+	}
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public float getEsal() {
+		return esal;
+	}
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+	public String getEadd() {
+		return eadd;
+	}
+	public void setEadd(String eadd) {
+		this.eadd = eadd;
+	}		
+}
